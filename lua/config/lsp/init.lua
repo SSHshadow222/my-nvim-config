bd91e28fc8d8d47878af85c1db4a325a3e3afee0
sshshadow2222 <shadow2die@outlook.com>
2026-04-27 00:21:51 +0300

    add keymap for code actions

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,9 +1,11 @@
 local path = vim.fn.stdpath("config") .. "/lua/config/lsp"
 
 for _, file in ipairs(vim.fn.readdir(path)) do
-  if file:sub(-4) == ".lua" and file ~= "init.lua" then
+  if file:sub(-4) == ".lua" and file ~= "init.lua" and file ~= "keymaps.lua" then
     local module = "config.lsp." .. file:gsub("%.lua$", "")
 
     require(module)
   end
 end
+
+require("config.lsp.keymaps")
---
+++ lua/config/lsp/keymaps.lua
@@ -0,0 +1,1 @@
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
