81c1c3ab872caa02d918786893e43f5df5f7ee18
sshshadow2222 <shadow2die@outlook.com>
2026-04-27 00:09:08 +0300

    add telescope extension ui for code actions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,14 +2,28 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   config = function()
+    -- Setting Keymaps for Telescope
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>ff", builtin.find_files)
     vim.keymap.set("n", "<leader>fg", builtin.live_grep)
     vim.keymap.set("n", "<leader>fb", builtin.buffers)
     vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+
+    -- Setting up Telescope and extensions
+    require("telescope").setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        }
+      }
+    })
+
+    -- Loading Telescope extensions
+    require("telescope").load_extension("ui-select")
   end
 }
